b368e028da3fb7f725a85048f967f8fb4d218cdd
PetyrB2 <peter.i.broadbent@gmail.com>
2022-10-24 12:56:03 +0100

    Added a few extra parts of the function

--- src/localDateTimeExample/localDateExample.java
+++ src/localDateTimeExample/localDateExample.java
@@ -1,24 +1,53 @@
 package localDateTimeExample;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.util.Date;
 
 public class localDateExample {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		// Current Date
+		// Current Date - NOW
 		LocalDate currentDate = LocalDate.now();
-		System.out.println("Current Date: " + currentDate );
-		
-		// 10 days from now
+		System.out.println("------------------------------------------------------");
+		System.out.println("\tDate and Time Revisited by Peter Broadbent");
+		System.out.println("------------------------------------------------------\n");
+		System.out.println("Current Date: \n\t" + currentDate );
+		System.out.println("------------------------------");
+		// 10 Days From Now
 		LocalDate after10days = currentDate.plusDays(10);
-		System.out.println("Ten Days from Now: " + after10days);
-	
+		System.out.println("\nTen Days from Now: \n\t" + after10days);
+		System.out.println("------------------------------");
 		// 1 Week Before now
 		LocalDate oneWeekBefore = currentDate.minusWeeks(1);
-		System.out.println("One Week Before Now: " + oneWeekBefore);
-			
+		System.out.println("\nOne Week Before Now: \n\t" + oneWeekBefore);
+		System.out.println("------------------------------");
+		// 5 Days From Now
+		System.out.println("\nDate Now: " + currentDate);
+		
+		currentDate = currentDate.plusDays(5);
+		System.out.println("\nDate with 5 days added to IMMUTABLE: \n" + currentDate);
+		
+		// My Birthday
+		LocalDate myBday = LocalDate.of(1901, Month.DECEMBER, 1);
+		System.out.println("\n------------------------------\n");
+		System.out.println("------> My Birthday <------- \n\t" + myBday);
+	
+		// isBefore - is my Bday Birthday NOW
+		System.out.println("\n------------------------------\n");
+		System.out.println("\n-------- Date Comparison ----------------\n");
+		System.out.println("Is Birthday before NOW: " + myBday.isBefore(currentDate));
+		
+		// =============
+		// Before JAVA 8 
+		// =============
+		System.out.println("\n------------------------------\n");
+		System.out.println("\n------- Date Before Java 8 -------\n");
+		Date myCurrentDate = new Date();
+		System.out.println("Date before Java 8: \n\t" + myCurrentDate);
+
 	}
 
 }
